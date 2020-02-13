3cdf8825f725184a04b8e2e254d5e58802a255c7
Grzegorz Boltuc <grzegorz.boltuc@gmail.com>
2020-02-13 16:50:44 +0100

    Add files via upload

---
+++ run_at_job.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Input variables
+TOWERURL=$1;
+TOWERLOGIN=$2;
+TOWERPASSWORD=$3;
+
+# Ansible Tower Credentials
+ATCRED="$TOWERLOGIN:$TOWERPASSWORD";
+
+# Ansible Tower Job Template ID
+ATJTID=35;
+
+echo "--";
+date;
+
+echo "Staring Ansible Tower job template ...";
+JOBID=`curl -s -k -X POST -u $ATCRED https://$TOWERURL/api/v2/job_templates/$ATJTID/launch/ | jq -r '.id'`;
+echo "Job id: $JOBID";
+
+JOBSTATUS=`curl -s -k -X GET -u $ATCRED https://$TOWERURL/api/v2/jobs/$JOBID/ | jq -r '.status'`;
+while [ $JOBSTATUS == "waiting" ] || [ $JOBSTATUS == "running" ] || [ $JOBSTATUS == "null" ];
+ do
+  sleep 5;
+  JOBSTATUS=`curl -s -k -X GET -u $ATCRED https://$TOWERURL/api/v2/jobs/$JOBID/ | jq -r '.status'`;
+ done
+
+function jobOutput {
+ echo "Job status: $JOBSTATUS";
+ echo "--";
+ echo "Job output:";
+ echo "";
+ curl -s -k -X GET -u $ATCRED https://$TOWERURL/api/v2/jobs/$JOBID/stdout/?format=txt;
+}
+
+if [ $JOBSTATUS == "successful" ]
+then
+ jobOutput;
+ echo "--";
+ date;
+ exit 0;
+else
+ jobOutput;
+ echo "--";
+ date;
+ exit 1;
+fi
+
+# end.
+
